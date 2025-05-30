72a5d9b650b7e719f5483d4c202b26f532b79302
kokun.v <vkokun@icloud.com>
2025-05-30 12:20:38 +0300

    GuestUserComponent

---
+++ src/main/java/com/greencity/ui/components/header/GuestUserComponent.java
@@ -0,0 +1,22 @@
+package com.greencity.ui.components.header;
+
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class GuestUserComponent extends HeaderComponent{
+
+    @Getter
+    @FindBy(className = "header_sign-in-link")
+    private WebElement signInButton;
+
+    public GuestUserComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    public void goToSignIn() {
+        waitUntilElementClickable(signInButton);
+        signInButton.click();
+    }
+}
