95eba48d5d3ac2e5d85cd89c2645e50ceab43787
Liubomyr Halamaha <galamagal86@gmail.com>
2025-05-30 17:06:55 +0300

    Update and rename GuestUserComponent.java to HeaderGuestUserComponent.java

--- src/main/java/com/greencity/ui/components/header/HeaderGuestUserComponent.java
+++ src/main/java/com/greencity/ui/components/header/HeaderGuestUserComponent.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class GuestUserComponent extends HeaderComponent{
+public class HeaderGuestUserComponent extends HeaderComponent{
 
     @Getter
     @FindBy(className = "header_sign-in-link")
