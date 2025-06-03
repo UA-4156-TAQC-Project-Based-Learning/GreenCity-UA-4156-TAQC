872f4139146e637eb00a84aba185735a239b07ed
Svitlana-Kovalova <svitlanakovalova1008@gmail.com>
2025-06-03 16:57:03 +0300

    add SignInModal.java

---
+++ src/main/java/com/greencity/ui/components/baseComponents/SignInModal.java
@@ -0,0 +1,95 @@
+package com.greencity.ui.components.baseComponents;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SignInModal extends BaseComponent {
+
+    public SignInModal(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    @FindBy(id = "email")
+    private WebElement emailInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement signInButton;
+
+    @FindBy(css = "#email-err-msg")
+    private WebElement emailError;
+
+    @FindBy(css = "#pass-err-msg")
+    private WebElement passwordError;
+
+    @FindBy(css = ".forgot-password")
+    private WebElement forgotPasswordLink;
+
+    @FindBy(css = "app-google-btn .google-sign-in")
+    private WebElement googleSignInButton;
+
+    @FindBy(css = ".green-link[aria-label='sign up modal window']")
+    private WebElement signUpLink;
+
+    @FindBy(css = ".mat-snack-bar-container span")
+    private WebElement snackBarSuccessMessage;
+
+    @FindBy(css = ".close-modal-window")
+    private WebElement closeButton;
+
+    public void clickSignIn() {
+        if (signInButton.isDisplayed() && signInButton.isEnabled()) {
+            signInButton.click();
+        }
+    }
+
+    public void enterEmail(String email) {
+        emailInput.clear();
+        emailInput.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickGoogleSignIn() {
+        googleSignInButton.click();
+    }
+
+    public void clickSignUp() {
+        signUpLink.click();
+    }
+
+    public void clickForgotPassword() {
+        forgotPasswordLink.click();
+    }
+
+    public void close() {
+        closeButton.click();
+    }
+
+    public String getEmailError() {
+        return emailError.getText().trim();
+    }
+
+    public String getPasswordError() {
+        return passwordError.getText().trim();
+    }
+
+    public String getSuccessMessage() {
+        return snackBarSuccessMessage.getText().trim();
+    }
+
+    public boolean isSignInButtonEnabled() {
+        return signInButton.isEnabled();
+    }
+
+    public boolean isDisplayed() {
+        return emailInput.isDisplayed() && passwordInput.isDisplayed();
+    }
+}
+
