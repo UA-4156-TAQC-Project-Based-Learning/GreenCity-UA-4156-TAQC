71bfc72cf786ca43fde11302c2f5ac3f1d8cdfbb
Liubomyr Halamaha <galamagal86@gmail.com>
2025-06-03 17:47:53 +0300

    impl LocalStorageJS

--- src/main/java/com/greencity/ui/pages/myspacepage/MySpacePage.java
+++ src/main/java/com/greencity/ui/pages/myspacepage/MySpacePage.java
@@ -1,9 +1,17 @@
 package com.greencity.ui.pages.myspacepage;
 
 import com.greencity.ui.pages.BasePage;
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MySpacePage extends BasePage {
+
+    @Getter
+    @FindBy(xpath = "//div[@class='description']/div[2]")
+    WebElement mySpaceDescription;
+
     public MySpacePage(WebDriver driver) {
         super(driver);
     }
