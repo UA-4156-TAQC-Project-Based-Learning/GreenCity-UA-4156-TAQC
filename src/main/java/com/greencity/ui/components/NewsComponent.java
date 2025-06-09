055e2806a722709902731e6f108ced546e492acb
Svitlana-Kovalova <svitlanakovalova1008@gmail.com>
2025-06-09 20:42:27 +0300

    add InterestingNewsWidgetTest.java

--- src/main/java/com/greencity/ui/components/NewsComponent.java
+++ src/main/java/com/greencity/ui/components/NewsComponent.java
@@ -7,39 +7,59 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 @Getter
 public class NewsComponent extends BaseComponent {
 
-//    public NewsComponent(WebDriver driver, WebElement rootElement) {
-//        super(driver, rootElement);
-//    }
+    @FindBy(xpath = "(.//div[@class='title-list word-wrap'])[1]")
+    private WebElement title;
 
-//    @FindBy(xpath = "(.//div[@class='title-list word-wrap'])[1]")
-//    private WebElement title;
-//
-//    @FindBy(xpath = "(.//div[@class='list-image-content'])[1]")
-//    private WebElement image;
-//
-//    @FindBy(xpath = ".//div[@class='filter-tag']")
-//    private List<WebElement> tags;
+    @FindBy(xpath = "(.//div[@class='list-image-content'])[1]")
+    private WebElement image;
 
+    @FindBy(xpath = ".//div[@class='filter-tag']")
+    private List<WebElement> tags;
 
-    private final WebElement title;
-
-    private final WebElement image;
-
-    private final List<WebElement> tags;
+    @FindBy(xpath = ".//p[contains(@class, 'user-data-text-date')]/span")
+    private WebElement publicationDate;
 
     public NewsComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
-        this.title = rootElement.findElement(By.xpath(".//div[@class='title-list word-wrap']"));
-        this.image = rootElement.findElement(By.xpath(".//img[@class='list-image-content']"));
-        this.tags = rootElement.findElements(By.xpath(".//div[@class='filter-tag']"));
     }
 
+//    private final WebElement title;
+//    private final WebElement image;
+//    @Getter
+//    private final List<WebElement> tags;
+//    private final WebElement publicationDate;
+//
+//    public NewsComponent(WebDriver driver, WebElement rootElement) {
+//        super(driver, rootElement);
+//        this.title = rootElement.findElement(By.xpath(".//div[@class='title-list word-wrap']"));
+//        this.image = rootElement.findElement(By.xpath(".//img[@class='list-image-content']"));
+//        this.tags = rootElement.findElements(By.xpath(".//div[@class='filter-tag']"));
+//        this.publicationDate = rootElement.findElement(By.xpath(".//p[contains(@class, 'user-data-text-date')]/span"));
+//    }
 
+    public String getTitleText() {
+        return title.getText().trim();
+    }
 
+    public LocalDate getPublicationDate() {
+        String dateText = publicationDate.getText().trim();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.ENGLISH);
+        return LocalDate.parse(dateText, formatter);
+    }
 
+    public void click() {
+        title.click();
+    }
 }
+
+
+
+
