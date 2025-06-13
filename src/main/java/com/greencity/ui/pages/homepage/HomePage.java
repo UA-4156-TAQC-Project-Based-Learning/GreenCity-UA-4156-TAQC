54e3b518a34aa4a9de0af6c8f3069b30d779e0ac
Svitlana-Kovalova <svitlanakovalova1008@gmail.com>
2025-06-13 23:03:24 +0300

    add AdvertisingInfoBlockTest.java

--- src/main/java/com/greencity/ui/pages/homepage/HomePage.java
+++ src/main/java/com/greencity/ui/pages/homepage/HomePage.java
@@ -2,6 +2,7 @@ package com.greencity.ui.pages.homepage;
 
 import com.greencity.ui.pages.BasePage;
 import com.greencity.ui.pages.econewspage.EcoNewsPage;
+import io.qameta.allure.Step;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,7 +10,6 @@ import org.openqa.selenium.support.FindBy;
 
 @Getter
 public class HomePage extends BasePage {
-
 
     @FindBy(xpath = "//section[@id='events']")
     private WebElement ecoNewsSection;
@@ -20,6 +20,17 @@ public class HomePage extends BasePage {
     @FindBy(xpath = "//a[@aria-label='link to eco-news page']")
     private WebElement readNewsLink;
 
+    @FindBy(css = "header#header h1")
+    private WebElement advertisingTitle;
+
+    @FindBy(css = "header#header p")
+    private WebElement advertisingText;
+
+    @FindBy(css = "header#header button.primary-global-button")
+    private WebElement startFormingHabitButton;
+
+    @FindBy(id = "guy-image")
+    private WebElement advertisingImage;
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -28,5 +39,44 @@ public class HomePage extends BasePage {
     public EcoNewsPage clickReadNewsLink() {
         readNewsLink.click();
         return new EcoNewsPage(driver);
+    }
+
+    @Step("Check if advertising title is displayed")
+    public boolean isAdvertisingTitleDisplayed() {
+        return advertisingTitle.isDisplayed();
+    }
+
+    @Step("Get advertising title text")
+    public String getAdvertisingTitleText() {
+        return advertisingTitle.getText().trim();
+    }
+
+    @Step("Check if advertising text is displayed")
+    public boolean isAdvertisingTextDisplayed() {
+        return advertisingText.isDisplayed();
+    }
+
+    public String getAdvertisingText() {
+        return advertisingText.getText();
+    }
+
+    @Step("Check if advertising image is displayed")
+    public boolean isAdvertisingImageDisplayed() {
+        return advertisingImage.isDisplayed();
+    }
+
+    @Step("Check if 'Start forming a habit!' button is visible")
+    public boolean isStartFormingHabitButtonDisplayed() {
+        return startFormingHabitButton.isDisplayed();
+    }
+
+    @Step("Get 'Start forming a habit!' button text")
+    public String getStartFormingHabitButtonText() {
+        return startFormingHabitButton.getText().trim();
+    }
+
+    @Step("Click on 'Start forming a habit!' button")
+    public void clickStartFormingHabitButton() {
+        startFormingHabitButton.click();
     }
 }
