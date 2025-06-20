49af268c39bccf37583cd8eb465e437183ad31bf
Liubomyr Halamaha <galamagal86@gmail.com>
2025-06-20 17:45:51 +0300

    update

--- src/main/java/com/greencity/api/clients/EcoNewsClient.java
+++ src/main/java/com/greencity/api/clients/EcoNewsClient.java
@@ -2,6 +2,12 @@ package com.greencity.api.clients;
 
 import com.greencity.api.models.econews.RequestEcoNews;
 import io.restassured.response.Response;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EcoNewsClient extends BaseClient {
 
@@ -19,6 +25,15 @@ public class EcoNewsClient extends BaseClient {
 
     }
 
+    public Response createNews(RequestEcoNews request, String imagePath) {
+        RequestSpecification requestSpecification = preparedRequest()
+                .contentType(ContentType.MULTIPART)
+                .multiPart("RequestEcoNews",request,"application/json");
+        attachImage(requestSpecification, imagePath);
+        return requestSpecification.post(recourseUrl);
+
+    }
+
     public Response getNewsById(int newsId) {
         return preparedRequest()
                 .get(recourseUrl + "/" + newsId);
@@ -28,5 +43,20 @@ public class EcoNewsClient extends BaseClient {
     public Response deleteNews(int newsId) {
         return preparedRequest()
                 .delete(recourseUrl + "/" + newsId);
+    }
+
+    private void attachImage(RequestSpecification requestSpecification, String imagePath) {
+        if (imagePath == null || imagePath.isEmpty()){
+            requestSpecification.multiPart("image", "", "");
+            return;
+        }
+        try {
+            File imageFile = new File(imagePath);
+            String fileName = imageFile.getName().toLowerCase();
+            String mimeType = fileName.endsWith(".png") ? "image/png" : fileName.endsWith(".gif")? "image/png": "image/jpeg";
+            requestSpecification.multiPart("image", fileName, new FileInputStream(imageFile), mimeType);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
